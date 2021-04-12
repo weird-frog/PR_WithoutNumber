09e16b2538f4793dbf71854865351ebaee00ecca
weird-frog <den.6667531@gmail.com>
2021-04-13 00:09:36 +0300

    Add project files.

---
+++ DNS/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите имя сайта в виде \"www.имя.домен\"\n(Например www.google.com)");
+
+                string hostname = Console.ReadLine(), ips = "", aliases = "";
+                IPHostEntry ipHostEntry = Dns.GetHostEntry(hostname);
+
+                foreach (IPAddress ip in ipHostEntry.AddressList)
+                {
+                    ips += ip.ToString() + "\n";
+                }
+
+                foreach (string aliasName in ipHostEntry.Aliases)
+                {
+                    aliases += aliasName + "\n";
+                }
+
+                if (aliases == "")
+                {
+                    Console.WriteLine("Псевдонимы: НЕТ");
+                }
+                else
+                {
+                    Console.WriteLine("Псевдонимы:\n" + aliases);
+                }
+
+                Console.WriteLine("\nСписок IP:\n" + ips);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong...\n{ex}");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
---
+++ Работа с несколькими проектами в рамках одного решения/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Работа_с_несколькими_проектами_в_рамках_одного_решения
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webclient = new WebClient();
+            try
+            {
+                Console.WriteLine("Скачивание архива .rar");
+                webclient.DownloadFile("https://caph.workupload.com/download/zMR4vIPW", "sample.rar");
+
+                Console.WriteLine("Скачивание файла .pdf");
+                webclient.DownloadFile("http://www.africau.edu/images/default/sample.pdf", "sample.pdf");
+
+                Console.WriteLine("Скачивание страницы Google\n");
+                Console.WriteLine(webclient.DownloadString("https://google.com"));
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
